c9d6678ab97a9f411a12ffa7b7eaf6289c1f9b37
Arow336 <Aasiyaharow@gmail.com>
2025-06-13 11:27:25 -0500

    Decision_structures_conditions.py

--- decision_structures_conditions.py
+++ decision_structures_conditions.py
@@ -24,16 +24,37 @@ def main():
     # on a test score 
     # A : 100 - 90, B: 89 - 80, C: 79- 70, D: 69 - 60, F less than 60
     print("Grade Decision: 1")
-    test_score = 80
+    test_score = 110
 
     if((test_score <= 100) and (test_score >= 90)): 
         print(f"{test_score} -- > A")
-    elif ((test_score <= 89) and (test_score >= 80)):
+    elif ((test_score >= 80)):
         print (f"{test_score} -- > B")
-    elif ((test_score <= 79) and (test_score >= 70)):
+    elif ((test_score >= 70)):
         print (f"{test_score} -- > C")
-    elif ((test_score <= 69) and (test_score >= 60)):
+    elif ((test_score >= 60)):
         print (f"{test_score} -- > D")
+    elif ((test_score < 0) or (test_score > 100)):
+         print (f"Invalid Test Score!")
     else: 
         print (f"{test_score} -- > F")
+
+    #Create a decision structure to determine the season: winter, spring, summer, fall 
+    # Ask the user to enter the number of the month and based on the number, determine the season
+    # Winter: 12, 1, 2; Spring: 3, 4, 5; Summer: 6, 7, 8; Fall: 9, 10, 11
+    #Output/print the season: It is season
+
+    season_no = int(input("What is the number of the month you are in? "))
+
+    if ((season_no == 1) or (season_no == 2) or (season_no == 12)):
+        print ("It is Winter")
+    elif ((season_no == 3) or (season_no == 4) or (season_no == 5)):
+        print ("It is Spring")
+    elif ((season_no == 6) or (season_no == 7) or (season_no == 8)):
+        print ("It is Summer")
+    elif ((season_no == 9) or (season_no == 10) or (season_no == 11)):
+        print ("It is Fall")
+    else: 
+        print ("That is not a valid month")
+    
 main()
